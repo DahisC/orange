0b6c9d785ed1fe98705894e59c4c120dd3f1f42a
DahisC <dahischeng@gmail.com>
2025-02-26 17:49:19 +0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -61,8 +61,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://nexifytw.mynetgear.com:45000',
-        changeOrigin: true,
+        target: 'http://nexifytw.mynetgear.com:45000/',
         secure: false, // 避免 HTTPS 強制要求
       }
     }
